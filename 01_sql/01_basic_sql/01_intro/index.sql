35c2cd8e83d019a527406deefb50d41c46d59e59
manojrawat87634 <manojrawat87634@gmail.com>
2026-07-07 12:51:52 +0530

    sharding in sql

---
+++ 01_sql/01_basic_sql/01_intro/index.sql
@@ -0,0 +1,12 @@
+create database school;
+
+show databases;
+use database school;
+
+create table student (name text, email text);
+create table teacher (name text, email text);
+
+
+insert into student (name, email) values ("abc", "abc@gmail.com"),
+("abc", "abc@gmail.com"),
+("abc", "abc@gmail.com");
---
+++ 01_sql/02_advance_sql/08_sharding/readme.md
@@ -0,0 +1,7 @@
+Why do we need Sharding?
+Sharding vs. Replication
+Sharding Keys and its types
+How to choose a Sharding key
+Routing & transaction
+Sharding pitfalls
+Sharding solutions
