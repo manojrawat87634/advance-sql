9569e4fc36fedcf9d022b8614f4d16aaa47d9415
manojrawat87634 <manojrawat87634@gmail.com>
2026-07-16 11:05:49 +0530

    cockroach db secondary index

--- 03_cockroach_db/04_secondary_index/index.sh
+++ 03_cockroach_db/04_secondary_index/index.sh
@@ -1,7 +1,8 @@
 # How to create secondary index
 CREATE INDEX idx_users_email ON users (email);
 
-
 # -- This duplicates the 'username' column directly onto the index disk space,
 # -- preventing the database from making a second network jump to the primary table node.
 CREATE INDEX idx_users_email_fast ON users (email) STORING (username);
+
+EXPLAIN SELECT username FROM users WHERE email = '1@gmail.com';
